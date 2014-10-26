7d8ffb9563db21ed0a2cbee17d090b1c7bc2e871
ildac <marco@dacol.me>
2014-10-26 22:37:18 +0100

    updated project description

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 Cloud BLE Sensor
 ================
 
-A simple framework to save on the cloud data from wireless BLE Arduino based sensors.
+A framework for smart environment.
+Developed in NodeJS exploiting the property of the streams.
+
+It is composed by 4 component:
+
+- arduino sensor with BLE shield
+- local nodeJS server mounted on a Rasberry(?)
+- cloud nodeJS restAPI server with a mongoDB database
+- android application to read the cloud data
+
+Right now it's beed developed using a macbookpro as local nodeJS server.
--- nodeAdapter/server.js
+++ nodeAdapter/server.js
@@ -1,5 +1,6 @@
+/*
 	Cloud BLE Sensor
-    Copyright (C) 2014  Marco Da Col
+	Copyright (C) 2014  Marco Da Col
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,3 +15,55 @@
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+var noble = require('noble');
+var connectedBean = null;
+
+// on startup start scanning for BLE devices
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning();
+    console.log('Scanning for BLE devices...');
+  } else {
+    noble.stopScanning();
+    console.log('State changed to ' + '. Scanning stopped.');
+  }
+});
+
+// when a device is discovered, noble connects to it
+noble.on('discover', function(peripheral) {
+  peripheral.connect(function(error) {
+    console.log('connected to peripheral: ' + peripheral.uuid);
+    peripheral.discoverServices(null, function(error, services) {
+      console.log('discovered the following services:');
+      for (var i in services) {
+        console.log('  ' + i + ' uuid: ' + services[i].uuid);
+      }
+    });
+  });
+  connectedBean = peripheral;
+});
+
+
+// to keep the server running
+process.stdin.resume();
+
+// to close gently
+var triedToExit = false;
+
+function exitHandler(options, err) {
+  if (connectedBean && !triedToExit) {
+    triedToExit = true;
+    console.log('Disconnecting...');
+    connectedBean.disconnect(function(err) {
+      console.log('Disconnected.');
+      process.exit();
+    });
+  } else {
+    process.exit();
+  }
+}
+
+// catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
